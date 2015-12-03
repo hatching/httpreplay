36d062bc3f53aadea4fe4eb99bf2e0e0a0553477
Jurriaan Bremer <jbr@cuckoo.sh>
2015-12-03 23:51:00 +0100

    allow directly importing all of the exported functions

--- httpreplay/__init__.py
+++ httpreplay/__init__.py
@@ -1,3 +1,18 @@
 # Copyright (C) 2015 Jurriaan Bremer <jbr@cuckoo.sh>
 # This file is part of HTTPReplay - http://jbremer.org/httpreplay/
 # See the file 'LICENSE' for copying permission.
+
+from .cobweb import HttpProtocol, SmtpProtocol
+from .cut import (
+    ForwardProtocol, dummy_handler, forward_handler, http_handler,
+    https_handler, tls_handler, smtp_handler,
+)
+from .exceptions import (
+    ReplayException, UnknownDatalink, UnknownEthernetProtocol,
+    UnknownIpProtocol, UnknownTcpSequenceNumber, InvalidTcpPacketOrder,
+    UnexpectedTcpData, UnknownHttpEncoding,
+)
+from .misc import read_tlsmaster
+from .reader import PcapReader
+from .shoddy import Protocol
+from .smegma import Packet, TCPPacketStreamer, TCPStream, TLSStream
