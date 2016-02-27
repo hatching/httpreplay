e43a156b5ec43f0a3fd8371eb79f463233badf33
Maximilian Hils <git@maximilianhils.com>
2016-02-27 03:14:13 +0100

    add pcap2mitmproxy

--- setup.py
+++ setup.py
@@ -15,11 +15,13 @@ setup(
     ],
     scripts=[
         "bin/httpreplay",
+        "bin/pcap2mitmproxy",
     ],
     license="GPLv3",
     description="Properly interpret, decrypt, and replay pcap files",
     install_requires=[
         "dpkt",
         "tlslite-ng",
+        "mitmproxy",
     ],
 )
