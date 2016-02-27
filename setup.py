7e5a11544f8aa5c0207852db6eaac313b70f5310
Jurriaan Bremer <jbr@cuckoo.sh>
2016-02-27 10:55:14 +0100

    version 0.1.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name="HTTPReplay",
-    version="0.1.6",
+    version="0.1.7",
     author="Jurriaan Bremer",
     author_email="jbr@cuckoo.sh",
     packages=[
