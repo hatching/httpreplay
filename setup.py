242719160b9b1e83a9e42aceebbe73421ffc0cf1
Kiran Tambe <kiran.tambe08@gmail.com>
2018-10-26 17:21:45 +0530

    Updated dpkt version to 1.9.1
    Older dpkt versions have installation issues on mac

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     license="GPLv3",
     description="Properly interpret, decrypt, and replay pcap files",
     install_requires=[
-        "dpkt==1.8.7",
+        "dpkt==1.9.1",
         "tlslite-ng==0.6.0",
         "click>=6.6, <7",
     ],
