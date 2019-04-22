4791c1fcec82bb954de78c3fafcd16aeca341e8c
LetMeR00t <alexandre.demeyer31@gmail.com>
2019-04-22 20:18:36 +0200

    Few changes to take into account a new kind of TLS Master keys

--- httpreplay/misc.py
+++ httpreplay/misc.py
@@ -3,15 +3,25 @@
 # See the file 'LICENSE' for copying permission.
 
 import re
+import codecs
+from builtins import str
 
-tlsmaster = "RSA Session-ID:(?P<sid>[0-9a-f]+) Master-Key:(?P<key>[0-9a-f]+)"
+decode_hex = codecs.getdecoder("hex_codec")
+
+tlsmaster1 = "RSA Session-ID:(?P<sid>[0-9a-f]+) Master-Key:(?P<key>[0-9a-f]+)"
+tlsmaster2 = "CLIENT_RANDOM (?P<sid>[0-9a-f]+) (?P<key>[0-9a-f]+)"
 
 def read_tlsmaster(filepath):
     ret = {}
-    for line in open(filepath, "rb"):
-        x = re.match(tlsmaster, line)
+    for line in open(filepath, "r"):
+        x = re.match(tlsmaster1, line)
         if x:
             sid = x.group("sid").strip()
             key = x.group("key").strip()
-            ret[sid.decode("hex")] = key.decode("hex")
+            ret[decode_hex(sid)[0]] = decode_hex(key)[0]
+        x = re.match(tlsmaster2, line)
+        if x:
+            sid = x.group("sid").strip()
+            key = x.group("key").strip()
+            ret[decode_hex(sid)[0]] = decode_hex(key)[0]
     return ret
